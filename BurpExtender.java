9f7166500a0536cefe05661c11e89c299f8c1ed5
Ivan Salosin <salonkasoli@gmail.com>
2016-12-30 18:38:56 +0700

    Prepare to send requests to host and port

--- BurpExtender.java
+++ BurpExtender.java
@@ -1,43 +1,59 @@
 package burp;
 
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.AbstractAction;
 import javax.swing.JMenuItem;
 
 
 public class BurpExtender implements IBurpExtender, IContextMenuFactory{
 
+	protected static final String HOST = "127.0.0.1";
+	protected static final String PORT = "8080";
 	private IBurpExtenderCallbacks callbacks;
+	private PrintWriter stdout;
+	private IExtensionHelpers helpers;
 	
-	
+
 	@Override
 	public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
 		
 		this.callbacks = callbacks;
-		
-		
-		callbacks.setExtensionName("REQUESTSENDER");
-		callbacks.registerContextMenuFactory(BurpExtender.this);
+		this.helpers = callbacks.getHelpers();
+        this.stdout = new PrintWriter(callbacks.getStdout(), true);
+        
+		callbacks.setExtensionName("REQUEST SENDER");
+		callbacks.registerContextMenuFactory(BurpExtender.this);		
 	}
-
 	
 	//IContextMenu Implementation
 	@Override
-	public List<JMenuItem> createMenuItems(IContextMenuInvocation invocation) {
+	public List<JMenuItem> createMenuItems(final IContextMenuInvocation invocation) {
 		
-		List<JMenuItem> menuitemlist = new ArrayList<JMenuItem>();
-		IHttpRequestResponse[] messages = invocation.getSelectedMessages();
-		if (messages.length > 0){
-			menuitemlist.add(new JMenuItem("TEST"));
+		List<JMenuItem> menuItemList = new ArrayList<JMenuItem>();
+		if ((invocation.getInvocationContext() == IContextMenuInvocation.CONTEXT_MESSAGE_VIEWER_REQUEST)
+				|| (invocation.getInvocationContext() == IContextMenuInvocation.CONTEXT_MESSAGE_EDITOR_REQUEST)){
+			JMenuItem item = new JMenuItem(new AbstractAction("SEND IT"){
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					IHttpRequestResponse[] requestArray = invocation.getSelectedMessages();
+					sendToAnotherHost(requestArray[0],HOST,PORT);
+				}
+				
+			});
+			menuItemList.add(item);
 		}
 		
 		
-		return menuitemlist;
+		return menuItemList;
 	}
-
 	
-	
-	
-
+	public void sendToAnotherHost(IHttpRequestResponse request, String host, String port){
+		stdout.println(host);
+		stdout.println(port);
+	}
 }
